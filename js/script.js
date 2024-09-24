cb43018eefb77fe3fb4d3bb8fba5a3e4755828f7
dominjk <dominik@salena.com.pl>
2024-09-24 11:33:37 +0200

    add logo animation (bugged)

--- js/script.js
+++ js/script.js
@@ -1,22 +1,20 @@
-const body = document.body
+
 
 
 function smooth_scroll (class_name, speed) {
-    const elem = document.getElementsByClassName(class_name)[0]
-    var offset = 0
+    const elem = $(`.${class_name}`)
+    let pos = 0
 
     function scroll_s() {
-        offset += (window.pageYOffset - offset) * speed
+        pos += (window.pageYOffset - pos) * speed
 
-        var scroll = `translateY(-${offset}px) translateZ(0)`
-        elem.style.transform = scroll
+        elem.css("transform", `translateY(-${pos}px) translateZ(0)`)
 
         callScroll = requestAnimationFrame(scroll_s)
     }
 
     scroll_s();
 }
-
 
 function texture_flash () {
     // funi (random texture flash)
@@ -36,24 +34,48 @@ $(window).on('beforeunload', function(){
 
 
 $( document ).ready(function() {
+    const tween = new TimelineLite()
+    const controller = new ScrollMagic.Controller()
+
     $(window).on('scroll', function() {
         var scroll_top = $(window).scrollTop()
-        // $(".fixed-scroll").css("top", `${Math.min(scroll_top**3 * -.000005, 2000)}px`)
-        // $(".logo").css("width", `${Math.max(70 - scroll_top**2 * .00005, 65)}vw`) <- size
-        // $(".logo").css("transform", `scale(${2 / scroll_top}, scale(${2 / scroll_top})`)
-        // console.log(2 / scroll_top)
-        // texture_flash()
 
-        if (scroll_top < 900) {
-            // fix mask position, release when scrolled 900px
+        if (scroll_top < 1200) {
+            // fix mask position, release when scrolled
             $(".mask").css("top", `${scroll_top}px`)
-        }
+            let blur = Math.max(Math.min((scroll_top - 850)/5, 100), 0)
+            $(".mask").css("filter", `blur(${blur}px)`)
+
+        } 
     });
     
 
-    smooth_scroll("fixed-scroll", .1)
-    smooth_scroll("social-media", .07)
-    // smooth_shrink("logo", 10)
-    
+    smooth_scroll("fixed-scroll", .07)
+    // smooth_scroll("social-media", .07)   
+
+    const flight_path = {
+        curviness: 2,
+        autoRotate: true,
+        values: [
+            {x: -200, y: 0},
+            {x: -500, y:0}
+
+        ]
+    }
+
+    tween.add(
+        TweenLite.to(".logo", 1, {
+            bezier: flight_path,
+            ease: Power1.easeInOut,
+            
+        })
+    )
+
+    const scene = new ScrollMagic.Scene ({
+        triggerElement: ".fixed-scroll",
+        duration: 2000,
+        triggerHook: 0
+    }).setTween(tween).addIndicators().addTo(controller);
+        
     
 });
