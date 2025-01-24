c311454c8e8acfaead65a5b9403e0f901fbcdef5
dominjk <dominik@salena.com.pl>
2025-01-24 12:12:50 +0100

    update

--- js/content-loader.js
+++ js/content-loader.js
@@ -1,99 +1,99 @@
 const shuffle_artworks = true
 const artworks = [
     {
-        "filename": "01.png",
+        "filename": "https://live.staticflickr.com/65535/54284724939_c653b8f064_c.jpg",
         "title": "untitled",
         "technique": "acrylic on canvas",
         "size": "60x80 cm"
     },
     {
-        "filename": "02.png",
+        "filename": "https://live.staticflickr.com/65535/54284910175_6ea5ea388b_c.jpg",
         "title": "untitled",
         "technique": "acrylic on canvas",
         "size": "50x70 cm"
     },
     {
-        "filename": "03.png",
+        "filename": "https://live.staticflickr.com/65535/54283597002_80f546bb43_z.jpg",
         "title": "untitled",
         "technique": "acrylic on canvas",
         "size": "50x70 cm"
     },
     {
-        "filename": "04.png",
+        "filename": "https://live.staticflickr.com/65535/54284910140_efa9ef1e55_z.jpg",
         "title": "untitled",
         "technique": "acrylic on canvas",
         "size": "50x70 cm"
     },
     {
-        "filename": "05.png",
+        "filename": "https://live.staticflickr.com/65535/54283597067_cc5c168d99_z.jpg",
         "title": "untitled",
         "technique": "oil on canvas",
         "size": "60x80 cm"
     }
     ,
     {
-        "filename": "06.png",
+        "filename": "https://live.staticflickr.com/65535/54284483376_f0948fa5ae_z.jpg",
         "title": "untitled",
         "technique": "oil on plywood",
         "size": "60x80 cm"
     },
     {
-        "filename": "07.png",
+        "filename": "https://live.staticflickr.com/65535/54284731298_d3406e42f9_z.jpg",
         "title": "untitled",
         "technique": "oil on canvas",
         "size": "50x70 cm"
     },
     {
-        "filename": "08.png",
+        "filename": "https://live.staticflickr.com/65535/54284731273_85f48e41a9.jpg",
         "title": "untitled",
         "technique": "acrylic on plywood",
         "size": "58x62 cm"
     },
     {
-        "filename": "09.jpg",
+        "filename": "https://live.staticflickr.com/65535/54284724864_1d05aa9551.jpg",
         "title": "untitled",
         "technique": "acrylic on canvas",
         "size": "60x80 cm"
     },
     {
-        "filename": "10.jpg",
+        "filename": "https://live.staticflickr.com/65535/54284724854_8a133034ee.jpg",
         "title": "untitled",
         "technique": "acrlic on canvas",
         "size": "50x50 cm"
     },
     {
-        "filename": "11.jpg",
+        "filename": "https://live.staticflickr.com/65535/54283596952_80b0216c3a.jpg",
         "title": "untitled",
         "technique": "acrylic on plywood",
         "size": "30x40 cm"
     }
     ,
     {
-        "filename": "12.jpg",
+        "filename": "https://live.staticflickr.com/65535/54283596932_eb0587a129.jpg",
         "title": "untitled",
         "technique": "acrylic on plywood",
         "size": "50x70 cm"
     },
     {
-        "filename": "13.png",
+        "filename": "https://live.staticflickr.com/65535/54284483311_aa80357796.jpg",
         "title": "untitled",
         "technique": "acrylic on plywood",
         "size": "30x40 cm"
     },
     {
-        "filename": "14.png",
+        "filename": "https://live.staticflickr.com/65535/54284724839_9de1c7dd64.jpg",
         "title": "untitled",
         "technique": "acrylic on canvas",
         "size": "50x50 cm"
     },
     {
-        "filename": "15.png",
+        "filename": "https://live.staticflickr.com/65535/54284483326_985745a56d.jpg",
         "title": "untitled",
         "technique": "acrylic on canvas",
         "size": "50x70 cm"
     },
     {
-        "filename": "16.png",
+        "filename": "https://live.staticflickr.com/65535/54284483276_79b95090bc.jpg",
         "title": "untitled",
         "technique": "acrylic on canvas",
         "size": "50x50 cm"
@@ -120,7 +120,7 @@ function load_content() {
 
     artworks.forEach(artwork => {
         let aw_div = $("<div>").addClass("artwork")
-        let img = $("<img>").attr("src", `https://dominik.works//artworks/${artwork.filename}`)
+        let img = $("<img>").attr("src", `${artwork.filename}`)
             .attr("alt", artwork.title)
             .attr("loading", "lazy")
         let title = $("<h4>").addClass("artwork-title").text(artwork.title)
@@ -144,5 +144,8 @@ function load_content() {
 }
 
 $(document).ready(function () {
-    // load_content()
+    load_content()
+    // fetch('./js/artworks.json')
+    //     .then((response) => response.json())
+    //     .then((json) => console.log(json));
 })
--- js/motion.js
+++ js/motion.js
@@ -38,6 +38,3 @@ $(document).ready(function () {
     }
 })
 
-Promise.all(Array.from(document.images).filter(img => !img.complete).map(img => new Promise(resolve => { img.onload = img.onerror = resolve; }))).then(() => {
-    console.log('images finished loading');
-});
